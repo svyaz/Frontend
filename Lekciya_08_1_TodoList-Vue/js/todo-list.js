d73bbd983f26d11fe85565dbe8ea1ee17d45ba9c
Max Kuzhelev <maxim.kuzhelev@gmail.com>
2019-08-11 02:08:44 +0700

    8.01 - Переделано с шаблонами.

--- Lekciya_08_1_TodoList-Vue/js/todo-list.js
+++ Lekciya_08_1_TodoList-Vue/js/todo-list.js
@@ -6,28 +6,57 @@ document.addEventListener("DOMContentLoaded", function () {
     var vm = new Vue({
         el: "#app",
         data: {
-            item: "",
+            newText: "",
             list: []
         },
         methods: {
-            focus: function() {
+            focus: function () {
                 this.$refs.addText.focus();
             },
+
             addItem: function () {
-                if (this.item === "") {
+                if (this.newText === "") {
                     this.showError(ERR_MSG_EMPTY_ITEM_TEXT);
                     return;
                 }
-                this.list.push(this.item);
-                this.item = "";
+                this.list.push({
+                    id: (new Date()).getTime(),
+                    text: this.newText
+                });
+                this.newText = "";
                 this.focus();
             },
-            deleteItem: function(index) {
-                this.list.splice(index, 1);
-                this.focus();
-            },
+
             showError: function (text) {
                 alert(text);
+            }
+        }
+    });
+
+    Vue.component("todo-list", {
+        data: function () {
+            return {
+                items: vm.list
+            };
+        },
+        template: "#todo-list-template",
+        methods: {
+            removeItem: function (item) {
+                this.items = this.items.filter(function (e) {
+                    return e.id !== item.id
+                });
+                vm.list = this.items;
+                vm.focus();
+            }
+        }
+    });
+
+    Vue.component("todo-item", {
+        props: ["item"],
+        template: "#todo-item-template",
+        methods: {
+            remove: function () {
+                this.$emit("remove-item", this.item);
             }
         }
     });
